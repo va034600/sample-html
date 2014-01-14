df41f0b7a9f9fbd5b40626751887d3435c6d0ed1
eisakuu <eisakuu@mac_sourcetree>
2014-01-14 21:06:35 +0900

    fundation5に対応

--- project-base/public/private/src/main/js/main.js
+++ project-base/public/private/src/main/js/main.js
@@ -1,10 +1,13 @@
 require([
+	"foundation",
 	"BaseSync",
 	"routers/MainRouter"
 ], function(
+	foundation,
 	BaseSync,
 	MainRouter
 ) {
+	$(document).foundation();
 	new MainRouter();
 	Backbone.history.start();
 });
--- project-base/public/private/src/main/js/require_config.js
+++ project-base/public/private/src/main/js/require_config.js
@@ -9,6 +9,9 @@ var require = {
 		'backbone' : {
 			deps : ['underscore', 'jquery'],
 			exports : 'Backbone'
+		},
+		'foundation' : {
+			deps : ['modernizr', 'jquery']
 		}
 	},
 	paths : {
@@ -16,7 +19,9 @@ var require = {
 		'underscore' : '../../lib/bower/js/underscore/underscore',
 		'backbone' : '../../lib/bower/js/backbone/backbone',
 		'text' : '../../lib/manual/js/require_text/text',
-		'i18n' : '../../lib/manual/js/require_i18n/i18n'
+		'i18n' : '../../lib/manual/js/require_i18n/i18n',
+		'foundation' : '../../lib/bower/js/foundation/foundation',
+		'modernizr' : '../../lib/bower/js/modernizr/modernizr'
 	},
 	urlArgs : 'bust=' + (new Date()).getTime()
 };
