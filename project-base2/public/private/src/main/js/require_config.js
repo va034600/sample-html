9262b843a8e4767e2c8cbeaf4b9a2edfff4e759b
va034600 <va034600@gmail.com>
2014-07-04 11:28:44 +0900

    requirejs導入

---
+++ project-base2/public/private/src/main/js/require_config.js
@@ -0,0 +1,11 @@
+var require = {
+  "shim": {
+    "angular": {
+      "exports": "angular"
+    }
+  },
+  "paths": {
+    "angular": "../../lib/bower/js/angular/angular"
+  },
+	urlArgs: 'bust=' + (new Date()).getTime()
+};
