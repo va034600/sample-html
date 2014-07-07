392adc3fcf0e3a34cc6f7c70731a17bf6409e6b2
va034600 <va034600@gmail.com>
2014-07-07 16:36:43 +0900

    router

--- project-base2/public/private/src/main/js/routes.js
+++ project-base2/public/private/src/main/js/routes.js
@@ -10,6 +10,25 @@ define(['MyModule'], function (app) {
 		$routeProvider.when('/view2', {
 			templateUrl: 'html/page2.html',
 			controller: function($scope, $location){
+				$('nav.affix-nav').affix({
+					offset:{
+						top: 300,
+						bottom:300
+					}
+				}).on('affix.bs.affix', function () {
+					$(this).css({
+						'top': '0'
+					});
+					console.log('affix.bs.affix');
+				}).on('affixed.bs.affix', function(){
+					console.log('affixed.bs.affix');
+				}).on('affix-bottom.bs.affix', function(){
+					console.log('affix-bottom.bs.affix');
+				}).on('affixed-bottom.bs.affix', function(){
+					console.log('affixed-bottom.bs.affix');
+				});
+
+				$('body').scrollspy({ target: 'nav.affix-nav' })
 			}
 		});
     }]);
