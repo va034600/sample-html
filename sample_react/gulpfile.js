2a5cbb041aae8844de14308308028a633e8a2cc1
va034600 <va034600@gmail.com>
2017-01-13 07:58:29 +0900

    sample react

--- sample_react/gulpfile.js
+++ sample_react/gulpfile.js
@@ -5,17 +5,10 @@ requireDir('./gulp/tasks', { recurse: true });
 
 var gulp = require('gulp');
 
-gulp.task('watch-start', function() {
-  gulp.watch('src/main/less/**/*.less', ["build-css"]);
-});
-
 gulp.task('build-js', function(callback) {
   runSequence('clean-js', 'webpack', callback);
 });
 
-gulp.task('build-css', function(callback) {
-  runSequence('clean-css', 'compile-sass', 'import-css', 'minify-css', callback);
-});
 
-gulp.task('build-all', ['build-js', 'build-css']);
-gulp.task('default', ['webpack-dev-server', 'watch-start']);
+gulp.task('build-all', ['build-js']);
+gulp.task('default', ['webpack-dev-server']);
