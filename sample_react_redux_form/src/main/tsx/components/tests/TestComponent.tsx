9635615c1b668ac0312c9527292f3ac92a61e53e
va034600 <va034600@gmail.com>
2017-01-16 08:31:06 +0900

    sample react redux form

--- sample_react_redux_form/src/main/tsx/components/tests/TestComponent.tsx
+++ sample_react_redux_form/src/main/tsx/components/tests/TestComponent.tsx
@@ -1,28 +1,20 @@
 import * as React from "react";
 import {connect} from "react-redux";
-import {Field, reduxForm, reducer as formReducer, FormProps, DataShape} from 'redux-form'
-
 import {TestGlobalState} from "../../states/tests/TestGlobalState";
-import {fetchTest, loadEvent} from "../../actions/tests/TestActions";
+import {mainFormComponentConnect} from "./TestMainFormComponent";
 
 interface Props {
     value: TestGlobalState;
     // dispatch: Redux.Dispatch<any>;
 };
 
-class TargetComponent extends React.Component<Props, {}> {
+class TestComponent extends React.Component<Props, {}> {
     constructor(props) {
         super(props);
     }
 
     render() {
-        const MainFormComponentRedux = reduxForm({
-            form: 'mainForm' // a unique name for this form
-        })(MainFormComponent);
-
-        const MainFormComponentConnect = connect(
-            mapStateToProps
-        )(MainFormComponentRedux);
+        const MainFormComponentConnect = mainFormComponentConnect();
 
         return (
             <div>
@@ -31,119 +23,6 @@ class TargetComponent extends React.Component<Props, {}> {
             </div>
         );
     }
-}
-
-interface MainDataShape{
-    // firstName?:string;
-    // lastName?:string;
-    // email?:string;
-//     favoriteColor?:string;
-}
-
-interface Props2 extends FormProps<MainDataShape, any, any>{
-    // value: TestGlobalState;
-    initialValues: any
-    dispatch: Redux.Dispatch<any>;
-};
-
-const renderField = ({ input, label, type, meta: { touched, error, warning } }) => (
-    <div>
-        <label>{label}</label>
-        <div>
-            <input {...input} placeholder={label} type={type}/>
-            {touched && ((error && <span>{error}</span>) || (warning && <span>{warning}</span>))}
-        </div>
-    </div>
-)
-
-class MainFormComponent extends React.Component<Props2, {}> {
-    render() {
-        const { error, handleSubmit, pristine, reset, submitting } = this.props;
-
-        const required = value => value ? undefined : 'Required'
-        const maxLength = max => value =>
-            value && value.length > max ? `Must be ${max} characters or less` : undefined
-        const maxLength15 = maxLength(15)
-        const number = value => value && isNaN(Number(value)) ? 'Must be a number' : undefined
-
-        return (
-            <form onSubmit={handleSubmit(this.submitAbc)}>
-                <Field name="username" type="text"
-                       component={renderField} label="Username"
-                       validate={[ required, maxLength15 ]}
-                />
-                <div>
-                    <label htmlFor="firstName">First Name</label>
-                    <Field name="firstName" component="input" type="text"/>
-                </div>
-                <div>
-                    <label htmlFor="lastName">Last Name</label>
-                    <Field name="lastName" component="input" type="text"/>
-                </div>
-                <div>
-                    <label htmlFor="email">Email</label>
-                    <Field name="email" component="input" type="email"/>
-                </div>
-                <div>
-                    <label>Sex</label>
-                    <div>
-                        <label><Field name="sex" component="input" type="radio" value="male"/> Male</label>
-                        <label><Field name="sex" component="input" type="radio" value="female"/> Female</label>
-                    </div>
-                </div>
-                <div>
-                    <label>Favorite Color</label>
-                    <div>
-                        <Field name="favoriteColor" component="select">
-                            <option></option>
-                            <option value="ff0000">Red</option>
-                            <option value="00ff00">Green</option>
-                            <option value="0000ff">Blue</option>
-                        </Field>
-                    </div>
-                </div>
-                <div>
-                    <label htmlFor="employed">Employed</label>
-                    <div>
-                        <Field name="employed" id="employed" component="input" type="checkbox"/>
-                    </div>
-                </div>
-                <div>
-                    <label>Notes</label>
-                    <div>
-                        <Field name="notes" component="textarea"/>
-                    </div>
-                </div>
-                <button type="submit" disabled={pristine || submitting}>Log In</button>
-                <button type="button" disabled={pristine || submitting} onClick={reset}>Clear Values</button>
-
-                <button type="button" onClick={this.clickByTestButton.bind(this)}>test</button>
-                <button type="button" onClick={this.clickByLoadButton.bind(this)}>load</button>
-            </form>
-        );
-    }
-
-    clickByTestButton(){
-        this.props.dispatch(fetchTest());
-    }
-
-    clickByLoadButton(){
-        this.props.dispatch(loadEvent({
-            firstName:"f1"
-        }));
-    }
-
-    submitAbc(values) {
-        console.log(values);
-        alert("do submit");
-    }
-}
-
-function mapStateToProps(state) {
-    return {
-        // value: state.testReducer
-        initialValues: state.testReducer
-    };
 }
 
 function mapStateToProps2(state) {
@@ -155,5 +34,5 @@ function mapStateToProps2(state) {
 export function test6(){
     return connect(
         mapStateToProps2
-    )(TargetComponent);
+    )(TestComponent);
 }
---
+++ sample_react_redux_form/src/main/tsx/components/tests/TestMainFormComponent.tsx
@@ -0,0 +1,128 @@
+import * as React from "react";
+import {connect} from "react-redux";
+import {Field, reduxForm, FormProps} from "redux-form";
+import {fetchTest, loadEvent} from "../../actions/tests/TestActions";
+
+interface MainDataShape{
+    // firstName?:string;
+    // lastName?:string;
+    // email?:string;
+//     favoriteColor?:string;
+}
+
+interface Props2 extends FormProps<MainDataShape, any, any>{
+    // value: TestGlobalState;
+    initialValues: any
+    dispatch: Redux.Dispatch<any>;
+};
+
+const renderField = ({ input, label, type, meta: { touched, error, warning } }) => (
+    <div>
+        <label>{label}</label>
+        <div>
+            <input {...input} placeholder={label} type={type}/>
+            {touched && ((error && <span>{error}</span>) || (warning && <span>{warning}</span>))}
+        </div>
+    </div>
+)
+
+class MainFormComponent extends React.Component<Props2, {}> {
+    render() {
+        const { error, handleSubmit, pristine, reset, submitting } = this.props;
+
+        const required = value => value ? undefined : 'Required'
+        const maxLength = max => value =>
+            value && value.length > max ? `Must be ${max} characters or less` : undefined
+        const maxLength15 = maxLength(15)
+        const number = value => value && isNaN(Number(value)) ? 'Must be a number' : undefined
+
+        return (
+            <form onSubmit={handleSubmit(this.submitAbc)}>
+                <Field name="username" type="text"
+                       component={renderField} label="Username"
+                       validate={[ required, maxLength15 ]}
+                />
+                <div>
+                    <label htmlFor="firstName">First Name</label>
+                    <Field name="firstName" component="input" type="text"/>
+                </div>
+                <div>
+                    <label htmlFor="lastName">Last Name</label>
+                    <Field name="lastName" component="input" type="text"/>
+                </div>
+                <div>
+                    <label htmlFor="email">Email</label>
+                    <Field name="email" component="input" type="email"/>
+                </div>
+                <div>
+                    <label>Sex</label>
+                    <div>
+                        <label><Field name="sex" component="input" type="radio" value="male"/> Male</label>
+                        <label><Field name="sex" component="input" type="radio" value="female"/> Female</label>
+                    </div>
+                </div>
+                <div>
+                    <label>Favorite Color</label>
+                    <div>
+                        <Field name="favoriteColor" component="select">
+                            <option></option>
+                            <option value="ff0000">Red</option>
+                            <option value="00ff00">Green</option>
+                            <option value="0000ff">Blue</option>
+                        </Field>
+                    </div>
+                </div>
+                <div>
+                    <label htmlFor="employed">Employed</label>
+                    <div>
+                        <Field name="employed" id="employed" component="input" type="checkbox"/>
+                    </div>
+                </div>
+                <div>
+                    <label>Notes</label>
+                    <div>
+                        <Field name="notes" component="textarea"/>
+                    </div>
+                </div>
+                <button type="submit" disabled={pristine || submitting}>Log In</button>
+                <button type="button" disabled={pristine || submitting} onClick={reset}>Clear Values</button>
+
+                <button type="button" onClick={this.clickByTestButton.bind(this)}>test</button>
+                <button type="button" onClick={this.clickByLoadButton.bind(this)}>load</button>
+            </form>
+        );
+    }
+
+    clickByTestButton(){
+        this.props.dispatch(fetchTest());
+    }
+
+    clickByLoadButton(){
+        this.props.dispatch(loadEvent({
+            firstName:"f1"
+        }));
+    }
+
+    submitAbc(values) {
+        console.log(values);
+        alert("do submit");
+    }
+}
+
+function mapStateToProps(state) {
+    return {
+        // value: state.testReducer
+        initialValues: state.testReducer
+    };
+}
+
+const MainFormComponentRedux = reduxForm({
+    form: 'mainForm' // a unique name for this form
+})(MainFormComponent);
+
+export function mainFormComponentConnect(){
+    return connect(
+        mapStateToProps
+    )(MainFormComponentRedux);
+}
+
