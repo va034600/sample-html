6b41b3066a2ab3d2f1bd647ad1b9de27739b0520
va034600 <va034600@gmail.com>
2018-03-10 08:26:24 +0900

    project base 3

--- project-base3/src/main/ts/view/router/mainRouter.ts
+++ project-base3/src/main/ts/view/router/mainRouter.ts
@@ -8,18 +8,21 @@ import {SampleBindComponent} from "../component/SampleBindComponent";
 import {SampleLoadComponent} from "../component/SampleLoadComponent";
 import {SampleModalButtonComponent} from "../component/SampleModalButtonComponent";
 import {SampleColumnComponent} from "../component/SampleColumnComponent";
+import EnvironmentUtility from "../../utility/EnvironmentUtility";
+
+const ROOT_PATH = EnvironmentUtility.isDevelopment() ? "/" : "/";
 
 export default new VueRouter({
     mode: 'history',
     routes:[
-        { path: "/", component:  SampleComponent},
-        { name: 'sample', path: '/sample', component: SampleComponent},
-        { name: 'sample-button', path: '/sample-button', component: SampleButtonComponent},
-        { name: 'sample-parameter', path: '/sample-parameter/:id', component: SampleParameterComponent},
-        { name: 'sample-bind', path: '/sample-bind', component: SampleBindComponent},
-        { name: 'sample-load', path: '/sample-load', component: SampleLoadComponent},
-        { name: 'sample-modal-button', path: '/sample-modal-button', component: SampleModalButtonComponent},
-        { name: 'sample-column', path: '/sample-column', component: SampleColumnComponent},
+        { name: 'top', path: ROOT_PATH, component:  SampleComponent},
+        { name: 'sample', path: `${ROOT_PATH}sample`, component: SampleComponent},
+        { name: 'sample-button', path: `${ROOT_PATH}sample-button`, component: SampleButtonComponent},
+        { name: 'sample-parameter', path: `${ROOT_PATH}sample-parameter/:id`, component: SampleParameterComponent},
+        { name: 'sample-bind', path: `${ROOT_PATH}sample-bind`, component: SampleBindComponent},
+        { name: 'sample-load', path: `${ROOT_PATH}sample-load`, component: SampleLoadComponent},
+        { name: 'sample-modal-button', path: `${ROOT_PATH}sample-modal-button`, component: SampleModalButtonComponent},
+        { name: 'sample-column', path: `${ROOT_PATH}sample-column`, component: SampleColumnComponent},
         { path: '*', component: PageNotFoundComponent},
     ]
 })
