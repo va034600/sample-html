0dd29e3776bc8d972af3eaee8caffe591a27f1af
va034600 <va034600@gmail.com>
2019-01-13 08:10:47 +0900

    npm publish

--- sample-npm-publish/fetch-main/src/index.js
+++ sample-npm-publish/fetch-main/src/index.js
@@ -1,6 +1,6 @@
 console.log("-----")
 
-var fetchSample = require("@va034600/fetch-sample")
+var fetchSample = require("@va034600/fetch-library")
 console.log(fetchSample("https://google.com"))
 
 console.log("-----")
